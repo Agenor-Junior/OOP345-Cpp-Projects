dfff4029a072d66e86f79ecb1d367bf622193858
Agenor Dionizio da Silva Junior <agenor.dsjunior@gmail.com>
2025-01-29 11:34:57 -0500

    add test data files and enhance main.cpp to display command-line arguments

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -1,6 +1,14 @@
 
-int main()
-{
+#include <iostream>
+
+
+int main(int argc, char** argv)
+{   
+    std::cout <<"Arguments is:"<<std::endl;
+
+    for(int i=0; i< argc;i++){
+        std::cout << argv[i]<<std::endl;
+    }
 
     return 0;
 }
